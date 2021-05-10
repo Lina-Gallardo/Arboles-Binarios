aae98f76c9161401d00291067d96e22755ee9908
Daniel Pizarro <danypd1012@gmail.com>
2021-05-10 04:21:29 -0500

    Metodos Recorridos y Altura

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-include ('ArbolBinario.php');
+include ('arbolBinario.php');
 session_start();
 if(!isset($_SESSION["Arbol"])){
   $_SESSION['Arbol'] = new ArbolBinario();
 }
+$respuesta = "";
 $action = isset($_POST['action'])? $_POST['action'] : "";
 switch ($action) {
   case 'Crear Arbol':
@@ -16,9 +17,34 @@ switch ($action) {
       echo "Guardado con exito";
     }
     break;
+  case 'Pre-Orden':
+    $_SESSION['Arbol']->inicializar();
+    $respuesta = $_SESSION['Arbol']->recorrerPreOrden($_SESSION['Arbol']->getRaiz());
+    $_SESSION['Arbol']->inicializar();
+    break;
+
+  case 'In-Orden':
+    $_SESSION['Arbol']->inicializar();
+    $respuesta = $_SESSION['Arbol']->recorrerInOrden($_SESSION['Arbol']->getRaiz());
+    $_SESSION['Arbol']->inicializar();
+    break;
+
+  case 'Post-Orden':
+    $_SESSION['Arbol']->inicializar();
+    $respuesta = $_SESSION['Arbol']->recorrerPostOrden($_SESSION['Arbol']->getRaiz());
+    $_SESSION['Arbol']->inicializar();
+    break;
+    
+    case 'Por Niveles':
+      $respuesta = $_SESSION['Arbol']->recorrerPorNiveles($_SESSION['Arbol']->getRaiz());
+      break;
+    
+    case 'Altura':
+      $respuesta = $_SESSION['Arbol']->Altura($_SESSION['Arbol']->getRaiz());
+      break;
 
   default:
-    if($_SESSION['Arbol']) $_SESSION['Arbol']->recorrerPreOrden($_SESSION['Arbol']->getRaiz());
+    if($_SESSION['Arbol']) echo $_SESSION['Arbol']->altura($_SESSION['Arbol']->getRaiz());
     break;
 }
 ?>
@@ -53,6 +79,18 @@ switch ($action) {
     </div>
     <div class="container" id="Arbol1">
 
+    </div>
+    <div class="container"> 
+      <form action="index.php" method="post">
+        <input type="submit" name="action" value="In-Orden">
+        <input type="submit" name="action" value="Pre-Orden">
+       <input type="submit" name="action" value="Post-Orden">
+       <input type="submit" name="action" value="Por Niveles">
+       <input type="submit" name="action" value="Altura">
+      </form>
+      <div class="mostrar">
+        <?php echo "".($respuesta? $respuesta:""); ?>
+      </div>
     </div>
     <script type="text/javascript">
       var nodos = new vis.DataSet([
